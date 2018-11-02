d4e1b780bff5707dbc8d87a07694e89a303585a3
Hiroshi Hatake <hatake@clear-code.com>
2018-11-02 14:46:38 +0900

    Extract endpoint placeholder
    Fix #16

--- lib/fluent/plugin/out_http.rb
+++ lib/fluent/plugin/out_http.rb
@@ -186,6 +186,7 @@ class Fluent::Plugin::HTTPOutput < Fluent::Plugin::Output
 
   def write(chunk)
     tag = chunk.metadata.tag
+    @endpoint_url = extract_placeholders(@endpoint_url, chunk.metadata)
     chunk.msgpack_each do |time, record|
       handle_record(tag, time, record)
     end
